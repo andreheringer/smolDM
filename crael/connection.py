5204b741b7d34c6468bc26c5a8e365aa818c9c41
Andre Heringer <andregomesh@dcc.ufmg.br>
2019-09-01 17:21:23 -0300

    Async acess to SQLite DB

--- crael/connection.py
+++ crael/connection.py
@@ -6,6 +6,7 @@
 """
 
 import os
+import logging as logger
 import aiosqlite as aiolite
 
 
@@ -26,52 +27,6 @@ class SQLDataBase:
             Properties:
                 _conn: The sqlite connection object
         """
-        self.__conn = conn
-
-    @property
-    async def conn(self):
-        """Connection proprety getter.
-
-            Returns: self.__conn
-        """
-        return self.__conn
-
-    @conn.setter
-    async def conn(self, signal: bool):
-        """Connection proprety setter
-
-            Parameters:
-                signal: boolean that changes the connection state,
-                        true -> open connection
-                        false -> close connection
-            Returns:
-                nothing
-        """
-        if signal is False:
-            self.__conn.close()
-
-        if signal is True:
-            try:
-                self.__conn = await aiolite.connect(self.DATABASE_URI)
-            except aiolite.Error as Err:
-                print(Err)
-                self.__conn = None
-        return None
-
-    async def __aenter__(self):
-        """
-            Defines the SQLDatabase entering context manager
-            Returns itself.
-        """
-        self.conn = True
-        return self
-
-    async def __aexit__(self, exc_type, exc_log, exc_tb):
-        """
-            Defines the SQLDatabase exiting context manager
-        """
-        self.conn = False
-        return False  # this propagates any exceptions inside the with block
 
     def get_schema_script(self):
         with open(self.SCHEMA_URI, mode="r") as sql:
@@ -79,13 +34,18 @@ class SQLDataBase:
         return script
 
     async def execute_script(self, script: str):
-        await self.__conn.execute(script)
-        # logger.debug("Executed sql script:\n\n{script}\n\n")
-        changes = await self.__conn.total_changes()
-        # logger.debug(f"{changes} lines changed")
-        return None
+        async with aiolite.connect(self.DATABASE_URI) as db:
+            logger.info("Executing sql script:\n\n{script}\n\n")
+            await db.execute(script)
+            changes = db.total_changes()
+            logger.info(f"{changes} lines changed")
+        return changes
 
     async def execute_query(self, query: str):
-        cursor = await self.__conn.execute(query)
-        rows = await cursor.fetchall()
+        
+        async with aiolite.connect(self.DATABASE_URI) as db:
+            logger.info(f"Executing query {query}")
+            async with db.execute(query) as cursor:
+                rows = await cursor.fetchall()
+        
         return rows
