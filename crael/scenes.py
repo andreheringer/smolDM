9e06b5093babbcc16319d04557adfe9855a97d3c
Andre Heringer <andregomesh@dcc.ufmg.br>
2020-03-19 18:32:03 -0300

    ADD: Docstrings to scenes.py

--- crael/scenes.py
+++ crael/scenes.py
@@ -1,37 +1,38 @@
 """
+This module defines the Scene related interfaces.
+
+:copywrite: Andre Heringer 2018-2019
+:license: MIT, see license for details
 """
 import re
 from dataclasses import dataclass
-from typing import List
+from typing import List, Dict
 
 
 @dataclass
 class Scene:
-    """
-    """
+    """Data class representing a crael Scene."""
 
     scene_id: int
     title: str
     lines: List[str]
     options: List[str]
 
-    def __str__(self):
-        return f"A sacene in the Crael game"
-
 
 class SceneLoader:
-    """
-    """
+    """This class loads scenes from markdown files."""
 
     def __init__(self):
-        """
+        """Initiaze SceneLoader.
+
+        The regular expressions match multiple scene attributes
         """
         # Thank you Ciça for the regex help
         self._scene_re = r"^\#\s(?:.|\n)*?\={5}$"
         self._scene_title_re = r"^\#.*\n"
-        self._scene_edge_re = r"^[1-9].\s"
+        self._scene_edge_re = r"^[1-99].\s"
 
-    def _parse_scene(self, scene_content, scene_num):
+    def _parse_scene(self, scene_content: str, scene_num: int) -> Scene:
         lines = list()
         options = list()
         for scene_att in scene_content.splitlines(True):
@@ -46,8 +47,15 @@ class SceneLoader:
             lines.append(scene_att)
         return Scene(scene_num, title, lines, options)
 
-    def load_scenes(self, scene_file_path):
-        """
+    def load_scenes(self, scene_file_path: str) -> Dict[int, Scene]:
+        """Load scenes from file path.
+
+        Args:
+            scene_file_path: mardown file with scene definitions
+
+        Returns:
+            scenes: Dictionary with numbered keys and Snece object values
+
         """
         scenes = dict()
         scene_file = open(scene_file_path, "r")
